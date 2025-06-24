d6c775106b4c68cc225c22c4e6354052bca72829
Luciano Torres <lucianotorreslanzavecchia@gmail.com>
2025-06-24 19:23:50 -0300

    empezando ejercicio 2

---
+++ LectorTagMP3/Id3v1Tag.cs
@@ -0,0 +1,13 @@
+namespace leerMp3
+{
+    class Id3v1Tag
+    {
+        
+        public string titulo { get; set; }
+        public string artista { get; set; }
+        public string album { get; set; }
+        public string anio { get; set; }
+        
+        
+    }
+}
---
+++ LectorTagMP3/Program.cs
@@ -0,0 +1,21 @@
+﻿
+bool existeArchivo;
+string ruta;
+do
+{
+    Console.WriteLine("Ingrese la direccion del archivo .mp3");
+    ruta = Console.ReadLine();
+    existeArchivo = File.Exists(ruta);
+    if (!existeArchivo)
+    {
+        Console.WriteLine("Ingrese una ruta valida");
+    }
+} while (!existeArchivo);
+byte[] buffer = new byte[128];
+using (FileStream archivoMp3 = new FileStream(ruta, FileMode.Open))
+{
+    archivoMp3.Read(buffer, 3, 93);
+    
+
+
+}
